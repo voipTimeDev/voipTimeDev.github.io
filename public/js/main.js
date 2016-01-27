4bbef0fda78a440e5b5995ab59041a6a9f88b5c7
dsavulyak <dsavulyak@voiptime.net>
2016-01-27 12:00:52 +0200

    add fixed navbar

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('danikkks');
+{
+  (function () {
+    var navbarFixed = document.getElementById('navbar-fixed');
+
+    window.onscroll = function () {
+      var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scrolled >= 53) {
+        navbarFixed.classList.add('navbar-fixed_visible');
+      } else if (scrolled <= 53) {
+        navbarFixed.classList.remove('navbar-fixed_visible');
+      }
+    };
+  })();
+}
--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,14 @@
-console.log('danikkks');
+{
+  let navbarFixed = document.getElementById('navbar-fixed');
+
+
+  window.onscroll = function() {
+    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrolled >= 53) {
+      navbarFixed.classList.add('navbar-fixed_visible');
+    } else if (scrolled <= 53) {
+      navbarFixed.classList.remove('navbar-fixed_visible');
+    }
+  }
+}
