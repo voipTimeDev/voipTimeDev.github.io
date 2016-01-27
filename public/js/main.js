6070df42991ac460fb2c9bd82a412e0f158a4d2c
dsavulyak <dsavulyak@voiptime.net>
2016-01-27 16:48:25 +0200

    rocket stagger

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,46 @@
 
 {
   (function () {
-    var navbarFixed = document.getElementById('navbar-fixed');
+    var rocketStagger = function rocketStagger() {
+      rocket.style.transition = 'all .2s';
+      rocket.style.top = '-5px';
+      rocket.style.right = '50px';
+      setTimeout(function () {
+        rocket.style.transition = 'all .4s';
+        rocket.style.top = '5px';
+        rocket.style.right = '30px';
+      }, 200);
+      setTimeout(function () {
+        rocket.style.transition = 'all .2s';
+        rocket.style.top = '0px';
+        rocket.style.right = '40px';
+      }, 600);
+    };
 
-    window.onscroll = function () {
+    var navbarFixed = document.getElementById('navbar-fixed');
+    var rocket = document.getElementById('rocket');
+    var getStartedBtn = document.getElementById('get-started');
+    var stopAnim = false;
+
+    getStartedBtn.onmouseover = function () {
+      rocketStagger();
+    };
+
+    getStartedBtn.onmouseleave = function () {
+      stopAnim = true;
+    };
+
+    window.onscroll = function scrollEvent() {
       var scrolled = window.pageYOffset || document.documentElement.scrollTop;
 
       if (scrolled >= 53) {
         navbarFixed.classList.add('navbar-fixed_visible');
       } else if (scrolled <= 53) {
         navbarFixed.classList.remove('navbar-fixed_visible');
+      }
+
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        rocket.classList.add('rocket_animation');
       }
     };
   })();
--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,19 @@
 {
   let navbarFixed = document.getElementById('navbar-fixed');
+  let rocket = document.getElementById('rocket');
+  let getStartedBtn = document.getElementById('get-started');
+  let stopAnim = false;
 
 
-  window.onscroll = function() {
+  getStartedBtn.onmouseover = function() {
+    rocketStagger();
+  }
+
+  getStartedBtn.onmouseleave = function() {
+    stopAnim = true;
+  }
+
+  window.onscroll = function scrollEvent() {
     let scrolled = window.pageYOffset || document.documentElement.scrollTop;
 
     if (scrolled >= 53) {
@@ -10,5 +21,28 @@
     } else if (scrolled <= 53) {
       navbarFixed.classList.remove('navbar-fixed_visible');
     }
+
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      rocket.classList.add('rocket_animation');
+    }
+  }
+
+  function rocketStagger() {
+    rocket.style.transition = 'all .2s';
+    rocket.style.top = '-5px';
+    rocket.style.right = '50px';
+    setTimeout(function() {
+      rocket.style.transition = 'all .4s';
+      rocket.style.top = '5px';
+      rocket.style.right = '30px';
+    }, 200);
+    setTimeout(function() {
+      rocket.style.transition = 'all .2s';
+      rocket.style.top = '0px';
+      rocket.style.right = '40px';
+    }, 600);
+
+
+
   }
 }
